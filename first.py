b1c80f7960fbfe00a145e4f7065c22827a4b58a8
anuragdw710 <78266752+anuragdw710@users.noreply.github.com>
2021-02-13 09:43:07 -0800

    add

--- first.py
+++ first.py
@@ -1,11 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("hello word")
-print("hoo")
-=======
-print("hello word in first.py")
->>>>>>> dev
-=======
-print("hello word in first.py")
-print("google ")
->>>>>>> dev
+print("its dev branch")
